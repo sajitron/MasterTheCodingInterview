94f7a89012b9a10323f4b9ac79e6d02854a0cbd3
Adams Banjo <sajioloye@gmail.com>
2023-04-10 13:18:33 +0000

    implement queues

---
+++ .build/queue.js
@@ -0,0 +1,36 @@
+"use strict";
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.first;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  dequeue() {
+  }
+}
+const newQueue = new Queue();
+newQueue.enqueue(46);
+newQueue.enqueue(96);
+console.log(JSON.stringify(newQueue));
+//# sourceMappingURL=queue.js.map
