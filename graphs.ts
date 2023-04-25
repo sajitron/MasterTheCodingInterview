657e4c36a688ab574e0600d6a876f5212e9820a3
sajitron <adams.b@roserocket.com>
2023-04-25 11:11:11 +0000

    add graph data structure

---
+++ graphs.ts
@@ -0,0 +1,19 @@
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {
+      
+    }
+  }
+
+  addVertex(node) {
+    this.adjacentList[node] = [];
+    this.numberOfNodes++;
+  }
+
+  addEdge(node1, node2) {
+    // undirected graph
+    this.adjacentList[node1].push(node2);
+    this.adjacentList[node2].push(node1);
+  }
+}
