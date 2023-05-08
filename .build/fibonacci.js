7af82021f20e5eac17b4b8f6fbb3510838caffb6
sajitron <adams.b@roserocket.com>
2023-05-08 11:07:17 +0000

    auto changes

--- .build/fibonacci.js
+++ .build/fibonacci.js
@@ -5,5 +5,11 @@ function fibonacciRecursive(n) {
   }
   return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 }
-fibonacciRecursive(3);
+function fibonacciIterative(n) {
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[n];
+}
 //# sourceMappingURL=fibonacci.js.map
--- .build/index.js
+++ .build/index.js
@@ -1,16 +1,33 @@
 "use strict";
-function fibonacciRecursive(n) {
-  if (n < 2) {
-    return n;
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
   }
-  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+  const length = array.length;
+  const middle = Math.floor(length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  );
 }
-function fibonacciIterative(n) {
-  let arr = [0, 1];
-  for (let i = 2; i < n + 1; i++) {
-    arr.push(arr[i - 2] + arr[i - 1]);
+function merge(left, right) {
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
   }
-  return arr[n];
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
-console.log(fibonacciIterative(8));
+mergeSort(numbers);
+console.log(numbers);
 //# sourceMappingURL=index.js.map
